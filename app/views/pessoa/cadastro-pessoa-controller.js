ebcdcefb20afbe1a22cf9af8d5bf047be07d7a3c
Itamar Veloso Rezende Junior <ivrj1980@gmail.com>
2016-01-19 21:58:52 -0200

    bla

---
+++ app/views/pessoa/cadastro-pessoa-controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Itamar on 19/01/2016.
+ */
+    angular.module('curso').controller('CadastroPessoaController',CadastroPessoaController);
+
+function CadastroPessoaController($scope){
+    $scope.nome = 'Controller  cadastro carregado';
+}
---
+++ app/views/pessoa/pesquisa-pessoa-controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Itamar on 19/01/2016.
+ */
+angular.module('curso').controller('PesquisaPessoaController',PesquisaPessoaController);
+
+function PesquisaPessoaController($scope){
+    $scope.nome = 'Controller da pesquisa carregado';
+}
